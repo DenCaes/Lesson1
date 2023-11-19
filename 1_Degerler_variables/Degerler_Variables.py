571b0af3aa9b6bf3f9d5a4691acc4de6fbff480e
DenCaes <119650796+DenCaes@users.noreply.github.com>
2023-11-19 10:35:40 +0300

    Add files via upload

---
+++ 1_Degerler_variables/Degerler_Variables.py
@@ -0,0 +1,25 @@
+
+
+
+# yorum satırı # ile belirtilir.
+
+#Basit deger atama
+
+number = 10  #integer
+
+site_name = 'programiz.pro' # String deger
+print(site_name)
+
+
+# Deger degistirme
+
+site_name = 'programiz.pro'
+print(site_name)
+
+#yeni deger atama
+site_name = 'apple.com'
+print(site_name)
+
+# Birden cok deger atama
+
+a, b, c = 5,3.2 ,'Hello' #int ,float ,String ifadeyei direkt atadik
